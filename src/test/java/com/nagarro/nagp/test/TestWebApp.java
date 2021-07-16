3b2a56ba91ba2378eeb46bd10fd5900348df61ca
ArshdeepSingh070 <79374651+ArshdeepSingh070@users.noreply.github.com>
2021-07-16 13:44:43 +0530

    change to test

--- src/test/java/com/nagarro/nagp/test/TestWebApp.java
+++ src/test/java/com/nagarro/nagp/test/TestWebApp.java
@@ -29,7 +29,7 @@ public class TestWebApp extends SpringBootHelloWorldTests {
 		mockMvc.perform(get("/employee")).andExpect(status().isOk())
 				.andExpect(content().contentType("application/json;charset=UTF-8"))
 				.andExpect(jsonPath("$.name").value("emp1")).andExpect(jsonPath("$.designation").value("manager"))
-				.andExpect(jsonPath("$.empId").value("1")).andExpect(jsonPath("$.salary").value(3000));
+				.andExpect(jsonPath("$.empId").value("1")).andExpect(jsonPath("$.salary").value(2000));
 
 	}
 
